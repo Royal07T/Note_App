ed9ddcf1dae3b666435b2f1225e28194dab1767c
sayi-victor <vctsayi@gmail.com>
2024-11-26 09:27:42 +0300

    fixed breeze installation issue

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
 
 
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body class="font-sans antialiased">
         @session('message')
 
         @endsession
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,43 @@
 <?php
 
 use App\Http\Controllers\NoteController;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/', [NoteController::class, 'index'])->name('note.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Index: Display a listing of the resource (homepage for this example)
-// Route::get('/', [NoteController::class, 'index'])->name('note.index');
+Route::controller(NoteController::class)
+ ->prefix('note')
+ ->name('notes.')
+ ->middleware(['auth'])
+ ->group(
+    function () {
+        // Index: Display a listing of the resource (homepage for this example)
+        Route::get('/', [NoteController::class, 'index'])->name('index');
+        // Create: Show the form for creating a new resource
+        Route::get('/create', 'create')->name('create');
+        // Store: Store a newly created resource in storage
+        Route::post('/notes', 'store')->name('store');
+        // Show: Display the specified resource
+        Route::get('/{id}', 'show')->name('show');
+        // Edit: Show the form for editing the specified resource
+        Route::get('/{id}/edit', 'edit')->name('edit');
+        // Update: Update the specified resource in storage
+        Route::put('/{id}', [NoteController::class, 'update'])->name('update');
+        // Destroy: Remove the specified resource from storage
+        Route::delete('/notes/{id}', 'destroy')->name('destroy');
+    });
 
-// // Create: Show the form for creating a new resource
-// Route::get('/notes/create', [NoteController::class, 'create'])->name('note.create');
-
-// // Store: Store a newly created resource in storage
-// Route::post('/notes', [NoteController::class, 'store'])->name('note.store');
-
-// // Show: Display the specified resource
-// Route::get('/notes/{id}', [NoteController::class, 'show'])->name('note.show');
-
-// // Edit: Show the form for editing the specified resource
-// Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
-
-// // Update: Update the specified resource in storage
-// Route::put('/notes/{id}', [NoteController::class, 'update'])->name('note.update');
-
-// // Destroy: Remove the specified resource from storage
-// Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
-
-
-Route::resource('notes', NoteController::class);
+require __DIR__.'/auth.php';
